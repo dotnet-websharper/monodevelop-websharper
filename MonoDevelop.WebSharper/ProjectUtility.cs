95a2a175506b6504d214927425dec07a15e5db42
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2014-04-09 07:41:03 +0000

    Fix to use embedded, static version of WebSharper for templates

--- MonoDevelop.WebSharper/ProjectUtility.cs
+++ MonoDevelop.WebSharper/ProjectUtility.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Xml;
 using Microsoft.FSharp.Core;
@@ -35,13 +36,19 @@ namespace MonoDevelop.WebSharper
 			{
 				var dir = info.ProjectBasePath.FullPath.ToString ();
 				var pkgs = info.SolutionPath.Combine ("packages").ToString ();
-				var opts = new T.NuGetOptions (pkgs, T.NuGetOptions.Create ().PackageSource);
-				var src = T.WebSharperSource.FromNuGet (None<string> (), Some<T.NuGetOptions> (opts));
-				var cfg = new T.InitOptions (dir, info.ProjectName, src);
+				var ns = new T.NuGetSource (ReadWebSharperPackage (), pkgs);
+				var cfg = new T.InitOptions (dir, info.ProjectName, T.Source.NuGet (ns));
 				template.Init (cfg);
 				var path = Directory.EnumerateFiles (dir, "*.*proj").First ();
 				CleanFsProj (path);
 				return (DotNetProject)Project.LoadProject (path, new NullProgressMonitor ());
+			}
+
+			private T.NuGetPackage ReadWebSharperPackage ()
+			{
+				using (var s = typeof(ProjectUtility).Assembly.GetManifestResourceStream ("WebSharper.nupkg")) {
+					return T.NuGetPackage.FromStream (s);
+				}
 			}
 
 			/// this is a hack around the fact that there are F#/Web templates that use
@@ -50,7 +57,7 @@ namespace MonoDevelop.WebSharper
 			private void CleanFsProj (string path)
 			{
 				if (Path.GetExtension (path) == ".fsproj") {
-					var lines = File.ReadLines (path).Where (line => !line.Contains ("<ProjectTypeGuids>"));
+					var lines = File.ReadAllLines (path).Where (line => !line.Contains ("<ProjectTypeGuids>"));
 					File.WriteAllLines (path, lines, new UTF8Encoding (false, false));
 				}
 			}
