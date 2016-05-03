dd01c3449b0fe0c0cb39555f02053e688cf8b160
András Jankó <andras.janko@gmail.com>
2016-05-03 17:49:54 +0200

    zafir-csharp initial commit

--- MonoDevelop.WebSharper/ProjectBindings.fs
+++ MonoDevelop.WebSharper/ProjectBindings.fs
@@ -4,41 +4,17 @@ module PU = ProjectUtility
 module T = WebSharper.Templates.All
 
 [<Sealed>]
-type BundleWebsiteProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.BundleWebsite)
+type CSharpBundleWebsiteProjectBinding() =
+    inherit PU.BaseProjectBinding(T.Template.CSharpBundleWebsite)
 
 [<Sealed>]
-type ExtensionProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.Extension)
+type CSharpLibraryProjectBinding() =
+    inherit PU.BaseProjectBinding(T.Template.CSharpLibrary)
 
 [<Sealed>]
-type LibraryProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.Library)
+type CSharpBundleUINextProjectBinding() =
+    inherit PU.BaseProjectBinding(T.Template.CSharpBundleUINext)
 
 [<Sealed>]
-type OwinSelfHostProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.OwinSelfHost)
-   
-[<Sealed>]
-type SiteletsHostProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.SiteletsHost)
-
-[<Sealed>]
-type SiteletsHtmlProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.SiteletsHtml)
-
-[<Sealed>]
-type SiteletsWebsiteProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.SiteletsWebsite)
-
-[<Sealed>]
-type BundleUINextProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.BundleUINext)
-
-[<Sealed>]
-type SiteletsUINextProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.SiteletsUINext)
-
-[<Sealed>]
-type SiteletsUINextSuaveProjectBinding() =
-    inherit PU.BaseProjectBinding(T.Template.SiteletsUINextSuave)
+type CSharpSiteletsUINextProjectBinding() =
+    inherit PU.BaseProjectBinding(T.Template.CSharpSiteletsUINext)
